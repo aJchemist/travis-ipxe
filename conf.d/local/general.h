bcce26d566b1ba7a0b4f8d2b7b2c6486202bb943
aJchemist <valchemist@me.com>
2016-12-28 18:53:56 +0900

    Changed patch to conf.d
    copy iPXE config overrides into source tree
    
    - conf.d/local -> ipxe/src/config/local

---
+++ conf.d/local/general.h
@@ -0,0 +1,9 @@
+#define DOWNLOAD_PROTO_NFS
+#define DOWNLOAD_PROTO_HTTPS
+#define PING_CMD
+#define CONSOLE_CMD
+#define REBOOT_CMD
+#define POWEROFF_CMD
+#define VLAN_CMD
+#define DIGEST_CMD				/* Image crypto digest commands */
+#define IMAGE_TRUST_CMD			/* Image trust management commands */
